34a0f9f3b83d7f13615d0c6f355aae84f46d003e
tanggaolin <tanggaolin123@163.com>
2019-04-09 18:00:05 +0800

    feat(*): Golang结构体内存对齐理解

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# go-test
+# golang语法学习测试代码
+
+[sync.once使用](once-demo.go)  
+[select使用](select-demo.go)  
+[用select & channel简单实现队列](queue-demo.go)
---
+++ memAlign.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Part1 struct {
+	a bool
+	b int32
+	c int8
+	d int64
+	e byte
+}
+
+// axxx|bbbb|cxxx|xxxx|dddddddd|exxx|xxxx
+
+type Part2 struct {
+	e byte
+	c int8
+	a bool
+	b int32
+	d int64
+}
+
+// ecax|bbbb|dddd|dddd
+
+func main() {
+	part1 := Part1{}
+	part2 := Part2{}
+	fmt.Printf("part1 size: %d, align: %d\n", unsafe.Sizeof(part1), unsafe.Alignof(part1))
+	fmt.Printf("part2 size: %d, align: %d\n", unsafe.Sizeof(part2), unsafe.Alignof(part2))
+}
+
+/****************
+为什么要做对齐
+* 平台（移植性）原因：不是所有的硬件平台都能够访问任意地址上的任意数据。例如：特定的硬件平台只允许在特定地址获取特定类型的数据，否则会导致异常情况
+* 性能原因：若访问未对齐的内存，将会导致 CPU 进行两次内存访问，并且要花费额外的时钟周期来处理对齐及运算。而本身就对齐的内存仅需要一次访问就可以完成读取动作
+
+默认系数
+在不同平台上的编译器都有自己默认的 “对齐系数”，可通过预编译命令 #pragma pack(n) 进行变更，n 就是代指 “对齐系数”。一般来讲，我们常用的平台的系数如下：
+* 32 位：4
+* 64 位：8
+
+对齐规则
+* 结构体的成员变量，第一个成员变量的偏移量为 0。往后的每个成员变量的对齐值必须为编译器默认对齐长度（#pragma pack(n)）或当前成员变量类型的长度（unsafe.Sizeof），取最小值作为当前类型的对齐值。其偏移量必须为对齐值的整数倍
+* 结构体本身，对齐值必须为编译器默认对齐长度（#pragma pack(n)）或结构体的所有成员变量类型中的最大长度，取最大数的最小整数倍作为对齐值
+* 结合以上两点，可得知若编译器默认对齐长度（#pragma pack(n)）超过结构体内成员变量的类型最大长度时，默认对齐长度是没有任何意义的
+*****************/
